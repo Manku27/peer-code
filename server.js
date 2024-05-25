671cc6ea5442f718cf78e841871c7fbb27cfe727
Mayank Jhunjhunwala <v-mayankj@microsoft.com>
2024-05-25 12:39:34 +0530

    move file serve api to its own router

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
 const app = express();
 const port = 8006;
-const { searchFiles } = require("./src/queryToFileMapping");
+const fileRoutes = require("./src/fileRouter");
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, "public")));
 
 // Endpoint to fetch JavaScript files
-app.get("/fetch-file", (req, res) => {
-  const query = req.query.q;
-
-  const root = path.join(__dirname, "./js-files");
-
-  const matchingFiles = searchFiles(root, query);
-
-  if (matchingFiles.length > 0) {
-    // Serve the first matching file found
-    const filePath = path.join(root, matchingFiles[0]);
-    const fileContent = fs.readFileSync(filePath, "utf-8");
-    res.json({ success: true, content: fileContent });
-  } else {
-    res.json({ success: false, message: "File not found" });
-  }
-});
+app.use(fileRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
