17ef7f2a50aad7691e2cae8d7b756968c7402163
Mayank Jhunjhunwala <v-mayankj@microsoft.com>
2024-05-25 19:29:33 +0530

    consume new search api

--- public/script.js
+++ public/script.js
@@ -1,20 +1,22 @@
-document
-  .getElementById("searchForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const query = document.getElementById("query").value;
+document.getElementById("searchForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const query = document.getElementById("query").value;
 
-    fetch(`/fetch-file?q=${query}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const resultDiv = document.getElementById("result");
-        if (data.success) {
-          resultDiv.innerHTML = `<pre><code class="javascript">${data.content}</code></pre>`;
-          document.querySelectorAll("pre code").forEach((block) => {
-            hljs.highlightBlock(block);
-          });
-        } else {
-          resultDiv.textContent = data.message;
-        }
-      });
-  });
+  fetch(`/search?query=${query}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const resultDiv = document.getElementById("result");
+      if (data.success) {
+        const formattedContent = data.content
+          .replace(/\\n/g, "\n")
+          .replace(/\\t/g, "\t")
+          .replace(/\\"/g, '"');
+        resultDiv.innerHTML = `<pre><code class="javascript">${formattedContent}</code></pre>`;
+        document.querySelectorAll("pre code").forEach((block) => {
+          hljs.highlightBlock(block);
+        });
+      } else {
+        resultDiv.textContent = data.message;
+      }
+    });
+});
