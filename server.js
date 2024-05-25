06857bff7874c5aeb98e97c66a41b3cb14e21e65
Mayank Jhunjhunwala <v-mayankj@microsoft.com>
2024-05-25 19:29:56 +0530

    add mongodb connection and remove file serving

--- server.js
+++ server.js
@@ -2,13 +2,27 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8006;
-const fileRoutes = require("./src/fileRouter");
+const schemaRoutes = require("./src/api");
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/peerCode", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Endpoint to fetch JavaScript files
-app.use(fileRoutes);
+// Endpoint for mongodb apis
+app.use(schemaRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
